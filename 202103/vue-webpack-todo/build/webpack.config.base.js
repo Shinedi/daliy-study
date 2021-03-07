a0b51d15345b4f6732df54a1182d077cae5b339f
fubodi <fubodi@jd.com>
2021-03-07 22:58:19 +0800

    vue

--- 202103/vue-webpack-todo/build/webpack.config.base.js
+++ 202103/vue-webpack-todo/build/webpack.config.base.js
@@ -7,17 +7,17 @@ const config = {
     target: "web",                                      //设置webpack的编译目标是web平台
     entry: path.join(__dirname,'../client/index.js'),         //声明js文件入口,__dirname就是我们文件的根目录,用join拼接
     output:{                                            //声明出口文件
-        filename: 'bundle.js',                          //将挂载的App全部打包成一个bundle.js,在浏览器中可以直接运行的代码  
+        filename: 'bundle.js',                          //将挂载的App全部打包成一个bundle.js,在浏览器中可以直接运行的代码
         path: path.join(__dirname,'../dist')               //bundle.js保存的位置
     },
     module:{                                            //因为webpack只能处理js文件,且只识别ES5的语法
         rules:[
-            {
-                test: /\.(vue|jsx|js)$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules/,
-                enforce: 'pre'
-            },                                     //所以针对不同类型的文件,我们定义不同的识别规则,最终目的都是打包成js文件
+            // {
+            //     test: /\.(vue|jsx|js)$/,
+            //     loader: 'eslint-loader',
+            //     exclude: /node_modules/,
+            //     enforce: 'pre'
+            // },                                     //所以针对不同类型的文件,我们定义不同的识别规则,最终目的都是打包成js文件
             {
                 test: /\.vue$/,
                 loader: 'vue-loader',                    //处理.vue文件
@@ -37,7 +37,7 @@ const config = {
                 use: [
                     {                                   //loader是可以配置选项的,如下options
                         loader: 'url-loader',           //url-loader实际上依赖于file-loader,file-loader处理完文件可以保存为一个文件供处理
-                        options: { 
+                        options: {
                             limit: 1024,                //url-loader的好处是可以加一个限制的大小,对于小图片,在范围内可直接将图片转换成base64码直接存放在js中,以减少http请求.
                             name: 'resources/[path][name].[hash:8].[ext]'        //输出文件的名字,[name] 文件原名,[ext]文件扩展名,可以将输出的资源放在统一的目录下
                         }
@@ -46,7 +46,7 @@ const config = {
             }
         ]
     }
-   
+
 }
 
 module.exports = config                                 //声明一个config的配置,用于对外暴露
